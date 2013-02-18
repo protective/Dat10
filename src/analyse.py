8daef8c7cc3c4bc69310a234ebbd46aaf66c12bd
Sabrine <sabrine@sabrine-Lenovo-G560.(none)>
2013-02-18 16:13:47 +0100

    analyse

---
+++ src/analyse.py
@@ -0,0 +1,27 @@
+import pg, sys, os, csv
+
+TID = sys.argv[1]
+
+USER = os.getlogin()
+DB = 'gps_can'
+TABLE = 'a_gps_can_data'
+
+con = pg.connect(dbname=DB, host='localhost', user=USER,passwd='F1ff')
+
+res = con.query("select extract('epoch' from age(timestamp, (select min	(timestamp) from " + TABLE + " where tid=" + TID + "))), totalconsumed-(select min(totalconsumed) from " + TABLE + " where tid=" + TID + 	"), speed, rpm, acceleration from " + TABLE + " where tid=" + TID + " order by timestamp;").getresult()
+
+output = open('images/' + TID + '_data.csv', 'wb')
+
+s = ""
+for r in res:
+	for i in r:
+		s += str(i) + " "
+	s+='\n'
+print >> output, s
+
+print "set terminal png size 1000,500;set y2tics;" 
+print "set output 'images/" + TID + "_FuelSpeed.png';"
+print "plot 'images/" + TID + "_data.csv' using 1:2 with lines title 'Fuel', 'images/" + TID + "_data.csv' using 1:3 with lines axes x1y2 title 'Speed'"
+
+print "set output 'images/" + TID + "_FuelAcc.png';"
+print "plot 'images/" + TID + "_data.csv' using 1:2 with lines title 'Fuel', 'images/" + TID + "_data.csv' using 1:5 with lines axes x1y2 title 'Acc'"
