06709b8580b07986d23f059b9168ab2371f33790
Sabrine <sabrine@sabrine-Lenovo-G560.(none)>
2013-02-19 14:43:32 +0100

    new files

---
+++ src/setDirty.py
@@ -0,0 +1,10 @@
+import random, time, math, pg, sys,os
+
+USER = os.getlogin()
+DB = 'gps_can'
+TABLE = 'a_gps_can_data'
+MIN_COUNT = 100
+
+print "Setting dirty flag on " + TABLE
+con = pg.connect(dbname=DB, host='localhost', user=USER,passwd='F1ff')
+con.query('alter table ' + TABLE + ' add column dirty bool; update ' + TABLE + ' set dirty=false where tid in (select tid from (select tid, count(tid) from ' + TABLE + ' group by tid)a where a.count < ' + MIN_COUNT + ');
