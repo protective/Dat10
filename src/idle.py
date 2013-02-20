d2d5432761540edf0369640647afaed013578132
Karsten <karstenjjakobsen@gmail.com>
2013-02-20 12:26:23 +0100

    idle working

--- src/idle.py
+++ src/idle.py
@@ -5,14 +5,16 @@ import os, pg , math, sys
 con = pg.connect(dbname='gps_can', host='localhost', user=os.getlogin(),passwd='F1ff')
 
 try:
-	con.query('alter table a_gps_can_data add column idle int not null default 0;')
+	con.query('alter table a_gps_can_data drop idle;')
 finally:
-	print 'Setting idle state'
-	con.query("update a_gps_can_data set idle = 1 where speed < 10 and rpm <= 900;")
-	print 'Setting stopped state'
-	con.query("update a_gps_can_data set idle = 2 where rpm = 0;")
-	print "Setting running state"
-	con.query("update a_gps_can_data set idle = 3 where idle != 1 and idle !=2;")
+	con.query('alter table a_gps_can_data add column idle int not null default 0;')
+
+print 'Setting idle state'
+con.query("update a_gps_can_data set idle = 1 where speed < 10 and rpm <= 900;")
+print 'Setting stopped state'
+con.query("update a_gps_can_data set idle = 2 where rpm = 0;")
+print "Setting running state"
+con.query("update a_gps_can_data set idle = 3 where idle != 1 and idle !=2;")
 	
 
 
