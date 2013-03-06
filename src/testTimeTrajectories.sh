9d319e4bf41abcb4cf645ce031eec6cdc0a75cc0
Sabrine <email>
2013-03-06 13:32:58 +0100

    more tests

--- src/testTimeTrajectories.sh
+++ src/testTimeTrajectories.sh
@@ -1,10 +1,17 @@
-python getTrajectories.py 20 0
-python getTrajectories.py 40 0
-python getTrajectories.py 60 0
-python getTrajectories.py 80 0
-python getTrajectories.py 100 0
-python getTrajectories.py 120 0
-python getTrajectories.py 140 0
-python getTrajectories.py 160 0
+#python getTrajectories.py 20 0
+#python getTrajectories.py 40 0
+#python getTrajectories.py 60 0
+#python getTrajectories.py 80 0
+python getTrajectories.py 90 0
+#python getTrajectories.py 100 0
+python getTrajectories.py 110 0
+#python getTrajectories.py 120 0
+python getTrajectories.py 130 0
+#python getTrajectories.py 140 0
+python getTrajectories.py 150 0
+#python getTrajectories.py 160 0
+python getTrajectories.py 170 0
+python getTrajectories.py 180 0
+python getTrajectories.py 190 0
 
 
