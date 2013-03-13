95dc4668aa2c5422236b26d5450dc04dbd0f85a8
Karsten <karstenjjakobsen@gmail.com>
2013-03-13 13:48:23 +0100

    export

--- src/exportWeka.py
+++ src/exportWeka.py
@@ -6,11 +6,8 @@ TABLE = 'trip_data'
 
 con = pg.connect(dbname=DB, host='localhost', user=USER,passwd='F1ff')
 #
-<<<<<<< HEAD
-res = con.query("""select vehicleid, idle_percentage, km_pr_l, acckm, stopngo, cruise_percentage, total_km, temperature_percentage,  
-=======
-res = con.query("""select vehicleid, idle_percentage, km_pr_l, acckm, acckmWeight, stopngo, cruise_percentage, total_km, 
->>>>>>> 674bfc2e2dc120153c3619195e276d820b5c71b6
+res = con.query("""select vehicleid, idle_percentage,idle_wo_tl_percentage, km_pr_l, acckm, stopngo, cruise_percentage, total_km, temperature_percentage,  
+
 	(case 
 		when km_pr_l < 4 then 'low' 
 		when km_pr_l >= 4 and km_pr_l< 8 then 'medium'
@@ -23,19 +20,15 @@ output = open('weka/' + TABLE + '.arff', 'wb')
 output.write("""@RELATION iris
 @ATTRIBUTE id	REAL
 @ATTRIBUTE idle	REAL
+@ATTRIBUTE idleWOTL	REAL
 @ATTRIBUTE fuel	REAL
 @ATTRIBUTE accel REAL
 @ATTRIBUTE accelW REAL
 @ATTRIBUTE stopngo REAL
 @ATTRIBUTE cruise REAL
 @ATTRIBUTE length REAL
-<<<<<<< HEAD
 @ATTRIBUTE temperature REAL
 @ATTRIBUTE class	{low, medium, high}
-=======
-@ATTRIBUTE class	{low, medium ,high}
->>>>>>> 674bfc2e2dc120153c3619195e276d820b5c71b6
-
 @DATA
 """)
 
