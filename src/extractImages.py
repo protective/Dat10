679686876745c2af9e26e1073613bb9c4ec4d5de
Sabrine <email>
2013-03-14 13:22:05 +0100

    work

--- src/extractImages.py
+++ src/extractImages.py
@@ -1,11 +1,13 @@
 import pg, sys, os, csv
 
-USER = 'd103'
+USER = 'sabrine'
 DB = 'gps_can'
 TABLE = 'trip_data'
 TYPE = sys.argv[1]
 
 con = pg.connect(dbname=DB, host='localhost', user=USER,passwd='F1ff')
+
+print "set terminal png size 1000,500;" 
 
 if TYPE == 'km_pr_l':
 	vehicles = con.query("select distinct vehicleid from " + TABLE + ";").getresult()
@@ -16,8 +18,7 @@ if TYPE == 'km_pr_l':
 		output = open('images/' + vid + '_' + TYPE + '_data.csv', 'wb')
 		for r in res:
 			print>> output, r[0]
-
-	print "set terminal png size 1000,500;set y2tics;" 
+ 
 	print "set output 'images/" + TYPE + "Trips.png';"
 	print "set ylabel 'km/l';"
 	print "set xlabel 'Trips';"
@@ -30,15 +31,13 @@ if TYPE == 'km_pr_l':
 	print s + "4 lw 2 notitle, 8 lw 2 notitle"
 
 elif TYPE == 'TimeTrips':
-	print "set terminal png size 1000,500;" 
 	print "set output 'images/" + TYPE + ".png';"
 	print "set ylabel 'Number of trips"
 	print "set xlabel 'Time??'"
 
 	print "plot 'data/noTrajectories.csv' with lines lw 3 notitle"
 
-elif TYPE == 'LengthTrips':
-	print "set terminal png size 1000,500;" 
+elif TYPE == 'LengthTrips': 
 	print "set output 'images/" + TYPE + ".png';"
 	print "set ylabel 'Number of trips"
 	print "set xlabel 'Minimum number of seqments'"
@@ -54,12 +53,35 @@ elif TYPE == 'TripLengthKml':
 	for r in res:
 		writer.writerow(r)
 
-	print "set terminal png size 1000,500;" 
+	
 	print "set output 'images/TripLengthKml.png';"
 	print "set ylabel 'km/l"
 	print "set xlabel 'km'"
 
 	print "plot 'data/tripLengthKml.csv' notitle"
+
+elif TYPE == 'idle2':
+	res = con.query("""
+	select round(idle_percentage*100), 
+		count(case when km_pr_l <=4 then 1 end)::float/count(*)*100 as low,
+		count(case when km_pr_l < 8 then 1 end)::float/count(*)*100 as medium,
+		100 as high  
+	from trip_data group by round order by round;
+	""").getresult()
+		
+	output = open('data/idle2.csv', 'wb')
+	writer = csv.writer(output, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	for r in res:
+		writer.writerow(r)
+	
+	print "set output 'images/idle2.png';"
+	print "set yrange[0:100]"
+	print "set key outside"
+	print """plot 
+		'data/idle2.csv' using 1:4 t \"High\" w filledcurves x1 linestyle 4, 
+		'data/idle2.csv' using 1:3 t \"Medium\" w filledcurves x1 linestyle 3, 
+		'data/idle2.csv' using 1:2 t \"Low\" w filledcurves x1 linestyle 2"""
+	
 
 else:
 	val = TYPE + ', km_pr_l as val, total_fuel'
@@ -85,7 +107,6 @@ else:
 	for r in res:
 		writer.writerow(r)
 
-	print "set terminal png size 1000,500;set y2tics;" 
 	print "set output 'images/" + TYPE + "Trips.png';"
 	print "set ylabel 'km/l';"
 	print "set xlabel '"+ TYPE + "';"
