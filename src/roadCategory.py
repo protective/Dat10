772eadf501af57e3955bb0b7d9ef7f5c8a8374f5
Karsten <karstenjjakobsen@gmail.com>
2013-03-20 15:21:13 +0100

    update

--- src/roadCategory.py
+++ src/roadCategory.py
@@ -1,6 +1,6 @@
 import pg , math, sys, os ,time
 
-USER = 'karsten'
+USER = 'd103'
 DB = "gps_can"
 QUERY_TABLE = "a_gps_can_data"
 MAP_TABLE = "osm_dk_20130214"
@@ -14,10 +14,30 @@ con.query("alter table " + QUERY_TABLE + " drop IF EXISTS roadCategory;")
 con.query('alter table ' + QUERY_TABLE + ' add roadCategory int;')
 
 
-s = 'explain update ' + QUERY_TABLE + ' set roadCategory = p from  (select (case when category in ( \'11\',\'12\')  then 1 when category in( \'13\',\'14\',\'15\',\'21\',\'31\') then 2 when category in (\'41\',\'42\',\'51\',\'63\') then 3 end) as p from ' + QUERY_TABLE + ' inner join ' + MAP_TABLE + ' on ' + MAP_TABLE + '.segmentkey=' + QUERY_TABLE + '.segmentkey)s;'
+s = 'update ' + QUERY_TABLE + ' set roadCategory = (case when category in ( \'11\',\'12\')  then 1 when category in( \'13\',\'14\',\'15\',\'21\',\'31\') then 2 when category in (\'41\',\'42\',\'51\',\'63\') then 3 end) from ' + MAP_TABLE + ' where ' + MAP_TABLE + '.segmentkey=' + QUERY_TABLE + '.segmentkey;'
 print s
 con.query(s)
 
+
+con.query("alter table " + TABLE + " drop IF EXISTS PMoterRoad;")
+con.query('alter table ' + TABLE + ' add PMoterRoad int;')
+con.query("alter table " + TABLE + " drop IF EXISTS PNormalRoad;")
+con.query('alter table ' + TABLE + ' add PNormalRoad int;')
+con.query("alter table " + TABLE + " drop IF EXISTS PSmallRoad;")
+con.query('alter table ' + TABLE + ' add PSmallRoad int;')
+
+
+s = 'update ' + TABLE + '  set PMoterRoad = p from (select tid, count(case when roadCategory=1 then 1 end)::float/count(*) as p from ' + QUERY_TABLE + ' where dirty is false group by tid)f where ' + TABLE + '.tid=f.tid;'
+print s
+con.query(s)
+
+s = 'update ' + TABLE + '  set PNormalRoad = p from (select tid, count(case when roadCategory=2 then 1 end)::float/count(*) as p from ' + QUERY_TABLE + ' where dirty is false group by tid)f where ' + TABLE + '.tid=f.tid;'
+print s
+con.query(s)
+
+s = 'update ' + TABLE + '  set PSmallRoad = p from (select tid, count(case when roadCategory=3 then 1 end)::float/count(*) as p from ' + QUERY_TABLE + ' where dirty is false group by tid)f where ' + TABLE + '.tid=f.tid;'
+print s
+con.query(s)
 
 #res = con.query('select speed, timestamp, tid  from ' + QUERY_TABLE + ' where tid in (select tid from ' + TABLE + ') and dirty is false order by tid, timestamp').getresult()
 
