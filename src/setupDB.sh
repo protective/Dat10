87ec6f55ef9d928252096f28c7e4430c6169cc8e
Sabrine <email>
2013-03-20 13:48:42 +0100

    update

--- src/setupDB.sh
+++ src/setupDB.sh
@@ -3,27 +3,27 @@ DB=$1
 FILEPATH=$2
 FILES=$FILEPATH/*.csv
 TABLE='gps_can_data'
-#psql -d template1 -c "drop database if exists $DB;"
-#psql -d template1 -c "create database $DB;"
+psql -d template1 -c "drop database if exists $DB;"
+psql -d template1 -c "create database $DB;"
 
-#psql -d $DB -c "Create EXTENSION postgis;"
+psql -d $DB -c "Create EXTENSION postgis;"
 
-#psql -d $DB -c "drop table IF EXISTS $TABLE;"
-#psql -d $DB -c "create table $TABLE (vehicleid bigint, timestamp timestamp, longitude float, latitude float, speed float, compass int, satellites int, temperature float, rpm int, acceleration float, kmcounter float, fuellevel float, throttlepos float, totalconsumed float, actualconsumed float, actual_km_l float, make float, model int, capacity float, weight float, segmentkey int, direction varchar(8));"
+psql -d $DB -c "drop table IF EXISTS $TABLE;"
+psql -d $DB -c "create table $TABLE (vehicleid bigint, timestamp timestamp, longitude float, latitude float, speed float, compass int, satellites int, temperature float, rpm int, acceleration float, kmcounter float, fuellevel float, throttlepos float, totalconsumed float, actualconsumed float, actual_km_l float, make float, model int, capacity float, weight float, segmentkey int, direction varchar(8));"
 
-#echo "Copying data"
-#for f in $FILES
-#do
-#psql -d $DB -c "\copy $TABLE from '$f' DELIMITERS ';' CSV HEADER;"
-#echo "Done loading $f"
-#done
+echo "Copying data"
+for f in $FILES
+do
+psql -d $DB -c "\copy $TABLE from '$f' DELIMITERS ';' CSV HEADER;"
+echo "Done loading $f"
+done
 
-echo "get trajectories TODO remember to set"
+echo "get trajectories"
 python getTrajectories.py 120 0
 
-#echo "Create geom postgis"
-#psql -d $DB -c "alter table a_gps_can_data add column geom geography(POINT,4326);"
-#psql -d $DB -c "update a_gps_can_data set geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);"
+echo "Create geom postgis"
+psql -d $DB -c "alter table a_gps_can_data add column geom geography(POINT,4326);"
+psql -d $DB -c "update a_gps_can_data set geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);"
 
 echo "Creating indexes"
 
