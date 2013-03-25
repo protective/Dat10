1a12248683ecceeb669ffecc84874db3501d91ba
Karsten <karstenjjakobsen@gmail.com>
2013-03-25 14:09:05 +0100

    test

--- src/setupTrips.sh
+++ src/setupTrips.sh
@@ -13,11 +13,13 @@ acceleration=true
 temperature=true
 
 
-#if ($postgis) then
-#echo "Create geom postgis"
-#psql -d $DB -c "alter table "$PREFIX"_gps_can_data add column geom geography(POINT,4326);"
-#psql -d $DB -c "update "$PREFIX"_gps_can_data set geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);"
-#psql -d $DB -c "DROP INDEX IF EXISTS idx_"$PREFIX"_gps_can_data_geom CASCADE; create index idx_"$PREFIX"_gps_can_data_geom on "$PREFIX"_gps_can_data using gist(geom);"
+if ($postgis) then
+echo "Create geom postgis"
+psql -d $DB -c "alter table "$PREFIX"_gps_can_data add column geom geography(POINT,4326);"
+psql -d $DB -c "update "$PREFIX"_gps_can_data set geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);"
+psql -d $DB -c "DROP INDEX IF EXISTS idx_"$PREFIX"_gps_can_data_geom CASCADE; create index idx_"$PREFIX"_gps_can_data_geom on "$PREFIX"_gps_can_data using gist(geom);"
+
+psql -d $DB -c "select geom from "$PREFIX" limit 1;"
 
 #if ($getTrajectories) then
 #echo "get trajectories"
