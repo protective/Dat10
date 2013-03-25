ff73ac4f509522f1fd30fae80d91c0c45b60b19d
Sabrine <email>
2013-03-25 09:15:58 +0100

    work

--- src/idleRange.py
+++ src/idleRange.py
@@ -12,6 +12,7 @@ if len(sys.argv) > 1:
 DATATABLE = ""+PREFIX+"_gps_can_data"
 TRIPDATA = ""+PREFIX+"_trip_data"
 
+"""
 print "Altering table"
 con.query('alter table ' + DATATABLE + ' drop IF EXISTS idleRange;')
 con.query('alter table ' + DATATABLE + ' add column idleRange int not null default 0;')
@@ -21,6 +22,13 @@ con.query('update ' + DATATABLE + ' set idleRange = 1 where rpm>0 and speed = 0;
 print "Creating index"
 con.query("DROP INDEX IF EXISTS idle_" + DATATABLE + "_idx;")
 con.query("Create index idle_" + DATATABLE + "_idx on " + DATATABLE + " (idleRange);")
+"""
+
+res = con.query("select timestamp from " + DATATABLE + " where idleRange=1 order by timestamp;").getresult()
+for r in res:
+	print r[0]
+
+
 
 if test:
 	print "Counting idle"
