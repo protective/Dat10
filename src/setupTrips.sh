e202fc60ac6ea922587050e7a23fe9b48777187c
Karsten <karstenjjakobsen@gmail.com>
2013-03-25 12:53:56 +0100

    ja

--- src/setupTrips.sh
+++ src/setupTrips.sh
@@ -17,7 +17,7 @@ if ($postgis) then
 echo "Create geom postgis"
 psql -d $DB -c "alter table "$PREFIX"_gps_can_data add column geom geography(POINT,4326);"
 psql -d $DB -c "update "$PREFIX"_gps_can_data set geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);"
-psql -d $DB -c "DROP INDEX IF EXISTS idx_"$PREFIX"_gps_can_data_geom CASCADE; create index idx_"$PREFIX"_gps_can_data_geom on "$PREFIX"_gps_can_data using gist(geom);")
+psql -d $DB -c "DROP INDEX IF EXISTS idx_"$PREFIX"_gps_can_data_geom CASCADE; create index idx_"$PREFIX"_gps_can_data_geom on "$PREFIX"_gps_can_data using gist(geom);"
 
 if ($getTrajectories) then
 echo "get trajectories"
