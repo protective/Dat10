ff9f5f4a58240c0da1e59f812d097d2ce04a59a9
Karsten <karstenjjakobsen@gmail.com>
2013-03-25 14:11:38 +0100

    test3

--- src/setupTrips.sh
+++ src/setupTrips.sh
@@ -20,6 +20,8 @@ psql -d $DB -c "update "$PREFIX"_gps_can_data set geom = ST_SetSRID(ST_MakePoint
 psql -d $DB -c "DROP INDEX IF EXISTS idx_"$PREFIX"_gps_can_data_geom CASCADE; create index idx_"$PREFIX"_gps_can_data_geom on "$PREFIX"_gps_can_data using gist(geom);"
 
 psql -d $DB -c "select geom from "$PREFIX" limit 1;"
+fi
+
 
 #if ($getTrajectories) then
 #echo "get trajectories"
