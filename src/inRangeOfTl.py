4595e9184b321f85b1bba1d52f41f84cd0fdd713
Karsten <karstenjjakobsen@gmail.com>
2013-04-02 10:31:03 +0200

    t

--- src/inRangeOfTl.py
+++ src/inRangeOfTl.py
@@ -36,15 +36,15 @@ for i in range(0,res[0][0]):
 
 #"explain update a_gps_can_data as a set tl = t.tlId from (select tlId from trafficlights where ST_Dwithin(geom,a.geom,100) )t"
 
-print 'Percentage in idle wo_tl'
-con.query('alter table ' + TRIPDATA + ' drop if exists idle_wo_tl_percentage;')
-con.query('alter table ' + TRIPDATA + ' add idle_wo_tl_percentage float;')
-con.query('update ' + TRIPDATA + ' set idle_wo_tl_percentage = p from (select tid, count(case when idle=1 and tl is null then 1 end)::float/count(*) as p from ' + DATATABLE + ' where dirty is false group by tid)f where ' + TRIPDATA + '.tid=f.tid;')
+#print 'Percentage in idle wo_tl'
+#con.query('alter table ' + TRIPDATA + ' drop if exists idle_wo_tl_percentage;')
+#con.query('alter table ' + TRIPDATA + ' add idle_wo_tl_percentage float;')
+#con.query('update ' + TRIPDATA + ' set idle_wo_tl_percentage = p from (select tid, count(case when idle=1 and tl is null then 1 end)::float/count(*) as p from ' + DATATABLE + ' where dirty is false group by tid)f where ' + TRIPDATA + '.tid=f.tid;')
 
-print 'Percentage in idle w_tl'
-con.query('alter table ' + TRIPDATA + ' drop if exists idle_w_tl_percentage;')
-con.query('alter table ' + TRIPDATA + ' add idle_w_tl_percentage float;')
-con.query('update ' + TRIPDATA + ' set idle_w_tl_percentage = p from (select tid, count(case when idle=1 and tl is not null then 1 end)::float/count(*) as p from ' + DATATABLE + ' where dirty is false group by tid)f where ' + TRIPDATA + '.tid=f.tid;')
+#print 'Percentage in idle w_tl'
+#con.query('alter table ' + TRIPDATA + ' drop if exists idle_w_tl_percentage;')
+#con.query('alter table ' + TRIPDATA + ' add idle_w_tl_percentage float;')
+#con.query('update ' + TRIPDATA + ' set idle_w_tl_percentage = p from (select tid, count(case when idle=1 and tl is not null then 1 end)::float/count(*) as p from ' + DATATABLE + ' where dirty is false group by tid)f where ' + TRIPDATA + '.tid=f.tid;')
 
 
 
