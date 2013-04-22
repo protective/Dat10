f76497053e2f3715e3201ae99ed489eb1131d12a
Karsten <karstenjjakobsen@gmail.com>
2013-04-22 10:49:55 +0200

    fix and updated setup trips to parallel

--- src/adjustSpeed.py
+++ src/adjustSpeed.py
@@ -13,6 +13,9 @@ except:
 	print 'Error: remember the parameters'
 	exit(1)
 
+def getTime(t):
+	return float(time.mktime(time.strptime(t, "%Y-%m-%j %H:%M:%S")))
+
 
 con.query('alter table ' + DATATABLE + ' drop IF EXISTS speedMod;')
 con.query('alter table ' + DATATABLE + ' add column speedMod integer not null default 0;')
@@ -22,10 +25,13 @@ counter = 0
 trips = con.query('select distinct tid from ' + DATATABLE +' where tid is not null').getresult()
 for t in trips:
 	trip = t[0]
-	res = con.query("select timestamp, speed, acceleration2 from " + DATATABLE + " where tid=" + str(trip) + " and dirty is false order by timestamp;").getresult()
+	res = con.query("select timestamp, speed from " + DATATABLE + " where tid=" + str(trip) + " and dirty is false order by timestamp;").getresult()
 	for r in range (1, len(res)-2):
-		acceleration = res[r][2]
-		if acceleration > 3 or acceleration < 10:
+		acc = 0
+		if r > 0:
+			acc = ((res[r][1]-res[r-1][1])/(getTime(curTime)-getTime(oldTime))/3.6)
+
+		if acc > 3 or acc < 10:
 			print res[r]
 			s1 = float(res[r-1][1])
 			s3 = float(res[r+1][1])
@@ -35,6 +41,7 @@ for t in trips:
 			newSpeed = s1 + (((s3-s1)/(t3-t1))*(t2-t1))
 			q = "update " + DATATABLE + " set speedMod = " + str(newSpeed) + " where timestamp = " + res[r][0] + " and tid=" + str(trip) + ";"
 			print q
+			con.query(q)
 			
 			
 			
