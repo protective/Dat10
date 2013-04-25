a6bc317500f0780cc7a2952482b0b68e205c1fcf
Sabrine <email>
2013-04-25 15:00:19 +0200

    comments

--- src/genImages.sh
+++ src/genImages.sh
@@ -46,11 +46,6 @@ scp d103@172.25.26.191:Dat10/src/images/idle2.png $PREFIX"_images"/idle2.png
 ssh -x -l d103 172.25.26.191 "cd Dat10/src/;python extractImages.py idle3 $TRIPS | gnuplot"
 scp d103@172.25.26.191:Dat10/src/images/idle3.png $PREFIX"_images"/idle3.png
 
-: << 'COMMENT'
-
-
-
-
 
 ssh -x -l d103 172.25.26.191 "cd Dat10/src/;python extractImages.py TripsKmlCluster $TRIPS | gnuplot"
 scp d103@172.25.26.191:Dat10/src/images/TripsKmlCluster.png $PREFIX"_images"/TripsKmlCluster.png
@@ -129,7 +124,7 @@ scp d103@172.25.26.191:Dat10/src/images/tlRange.png images/tlRange.png
 
 ssh -x -l d103 172.25.26.191 "cd Dat10/src/;python extractImages.py testRoad $TRIPS | gnuplot"
 scp d103@172.25.26.191:Dat10/src/images/testRoad.png $PREFIX"_images"/testRoad.png
-
+: << 'COMMENT'
 COMMENT
 
 
